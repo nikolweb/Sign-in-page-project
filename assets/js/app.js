3977bcedf0b8e63181e90e245ae5a40bd128988f
Nikolweb <nikolweb.ir@gmail.com>
2024-07-19 20:44:50 +0330

    create project files

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+function clickchange(){
+    document.getElementById("sub-btn").style.backgroundColor="green";
+    var btn=document.getElementById("sub-btn");
+    btn.value="done!";
+}
+
+
+function onmouseov(){
+    document.getElementById("card-text").innerHTML="سلام";
+}
+
+
+function onchangefunc(){
+    document.getElementById("fullname").style.borderColor="green";
+}
+
+function load_a(){
+    alert("خوش آمدید");
+}
+
+
+function copyed(){
+    alert("متن شما کپی شد!");
+}
